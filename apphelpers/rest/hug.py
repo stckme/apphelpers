eca713d894b489756d74b14b413eb4213bc03596
Shekhar <pythonic@gmail.com>
2019-04-07 19:28:21 +0530

    added convenience function for easier REST endpoint creation

--- apphelpers/rest/hug.py
+++ apphelpers/rest/hug.py
@@ -36,3 +36,27 @@ class APIFactory:
         def _wrapper(f):
             return self.router.delete(*a, **k)(self.build(f))
         return _wrapper
+
+    def map_resource(self, url, resource=None, handlers=None, id_field='id'):
+        if resource:
+            raise NotImplementedError("Resource not supported yet")
+
+        collection_url = (self.urls_prefix + url) if not url.startswith('/') else url
+        resource_url = collection_url + '{' + id_field + '}'
+
+        assert isinstance(handlers, (list, tuple)), "handlers should be list or tuple"
+        get_collection, add_resource, replace_resource, get_resource, update_resource, delete_resource = handlers
+
+        if get_collection:
+            self.get(collection_url)(get_collection)
+        if add_resource:
+            self.post(collection_url)(add_resource)
+        if replace_resource:
+            self.put(resource_url)(replace_resource)
+        if get_resource:
+            # get_resource_wrapped = get_or_not_found(get_resource)
+            self.get(resource_url)(get_resource)
+        if update_resource:
+            self.patch(resource_url)(update_resource)
+        if delete_resource:
+            self.delete(resource_url)(delete_resource)
