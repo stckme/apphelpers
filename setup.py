e6ad21b7524743b0ddf8c1dc7c3d7bf68d78c084
reck <rakesh.gunduka@gmail.com>
2019-05-02 16:42:26 +0530

    Requirements file removed and updated setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = open('requirements.txt').read().split()
+requirements = open('requirements_dev.txt').read().split()
 
 setup_requirements = [ ]
 
