283452ed7e536e17810e6cf896fd84db9fe820cd
Shekhar <pythonic@gmail.com>
2019-05-20 14:51:35 +0530

    Bump version: 0.9.1 → 0.9.2

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/scrolltech/apphelpers',
-    version='0.9.1',
+    version='0.9.2',
     zip_safe=False,
 )
