c7ca95dfc9a1344b25dab4af6aa18853e1af0f61
Shekhar <pythonic@gmail.com>
2019-11-11 18:54:28 +0530

    - Added missing __init__ - Used black to code format email module

--- apphelpers/utilities/email.py
+++ apphelpers/utilities/email.py
@@ -7,39 +7,43 @@ from email.mime.image import MIMEImage
 
 import settings
 
-def send_email(sender, recipient, subject, text=None, html=None, images=[], reply_to=None, bcc=None):
+
+def send_email(
+    sender, recipient, subject, text=None, html=None, images=[], reply_to=None, bcc=None
+):
     """
     text: text message. If html is provided and not text, text will be auto generated
     html: html message
-    images: list of cid and image paths. eg. [('logo', 'images/logo.png'), ('Bruce', 'images/bat.png')]
+    images: list of cid and image paths.
+        eg. [('logo', 'images/logo.png'), ('Bruce', 'images/bat.png')]
     """
-    assert any((text, html)), 'please provide html or text'
+    assert any((text, html)), "please provide html or text"
 
     if html and not text:
         text = html2text.html2text(html)
 
-    msg = MIMEMultipart('alternative')
+    msg = MIMEMultipart("alternative")
 
-    msg['Subject'] = subject
-    msg['From'] = sender
-    msg['To'] = recipient
+    msg["Subject"] = subject
+    msg["From"] = sender
+    msg["To"] = recipient
     if bcc:
-        msg['bcc'] = bcc
+        msg["bcc"] = bcc
     if reply_to:
-        msg.add_header('reply-to', reply_to)
+        msg.add_header("reply-to", reply_to)
 
-    msg.attach(MIMEText(text, 'plain', 'utf-8'))
+    msg.attach(MIMEText(text, "plain", "utf-8"))
     if html:
-        msg.attach(MIMEText(html, 'html', 'utf-8'))
+        msg.attach(MIMEText(html, "html", "utf-8"))
     for cid, img in images:
         img_part = MIMEImage(img)
-        img_part.add_header('Content-ID', '<' + cid + '>')
+        img_part.add_header("Content-ID", "<" + cid + ">")
         msg.attach(img_part)
 
     s = smtplib.SMTP(settings.MD_HOST, settings.MD_PORT)
     if settings.MD_USERNAME:
         s.login(settings.MD_USERNAME, settings.MD_KEY)
 
-    s.sendmail(msg['From'], msg['To'], msg.as_string())
+    s.sendmail(msg["From"], msg["To"], msg.as_string())
 
     s.quit()
