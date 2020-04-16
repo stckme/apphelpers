f45c13b2b429fe0b5081a2abf486a7131fedd742
Shekhar <pythonic@gmail.com>
2020-04-16 12:14:37 +0530

    Bump version: 0.11.1 → 0.12.0

--- apphelpers/__init__.py
+++ apphelpers/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """Scroll Tech"""
 __email__ = 'tech@scroll.in'
-__version__ = '0.11.1'
+__version__ = '0.12.0'
--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/scrolltech/apphelpers',
-    version='0.11.1',
+    version='0.12.0',
     zip_safe=False,
 )
