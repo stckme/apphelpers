51026e09f5914894bbe91007171cc1dc3893687a
Milind_suryawashi <milind_suryawanshi@scrollstack.com>
2021-01-11 10:01:08 +0530

    error handling for fb

--- apphelpers/fb.py
+++ apphelpers/fb.py
@@ -2,10 +2,15 @@ import os
 from requests_oauthlib import OAuth2Session
 
 from converge import settings
+from errors import UserAccountExistsError
 
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = 'True'
+
 
 def fetch_info(access_token):
     session = OAuth2Session(token={'access_token': access_token})
     info_url = 'https://graph.facebook.com/me?fields=' + settings.FB_USER_FIELDS
-    return session.get(info_url).json()
+    session_info = session.get(info_url).json()
+    if session_info['email'] is None:
+        raise UserAccountExistsError(session_info)
+    return session_info
