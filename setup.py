a6f9e38bc4fc9aab6f9da0dccfc4da219ac13b28
Mandar Vaze <mandarvaze@gmail.com>
2021-09-01 11:51:35 +0530

    Install loguru automatically with apphelpers

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('CHANGELOG.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = ['loguru']
 
 setup_requirements = [ ]
 
