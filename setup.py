2ae37eb21c94270b9d25cc9c9b04fb95b09304cd
Mandar Vaze <mandarvaze@gmail.com>
2021-09-01 11:58:57 +0530

    Bump version: 0.18.0 → 0.19.0

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/scrolltech/apphelpers',
-    version='0.18.0',
+    version='0.19.0',
     zip_safe=False,
 )
