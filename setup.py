2ae0c9206c631ea5942024a00755f2d858de9d3f
Shekhar <pythonic@gmail.com>
2022-06-15 15:05:43 +0530

    Bump version: 0.21.2 → 0.21.3

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/scrolltech/apphelpers',
-    version='0.21.2',
+    version='0.21.3',
     zip_safe=False,
 )
