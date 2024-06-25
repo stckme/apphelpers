34f0b71bd4fc44a14bec1029d899701189a60596
Arijit Basu <sayanarijit@gmail.com>
2024-06-25 16:24:54 +0530

    Support google jwt auth
    Ref: https://github.com/stckme/tango/issues/997

--- apphelpers/socialauth/goog.py
+++ apphelpers/socialauth/goog.py
@@ -1,5 +1,7 @@
 import os
 
+from google.auth.transport import requests
+from google.oauth2 import id_token
 from requests_oauthlib import OAuth2Session
 
 try:
@@ -11,7 +13,15 @@ if not settings.G_REDIRECT_URI.startswith("https"):
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "True"
 
 
-def fetch_info(access_token):
-    session = OAuth2Session(token={"access_token": access_token})
-    userinfo = session.get("https://www.googleapis.com/oauth2/v1/userinfo").json()
+def fetch_info(access_token, jwt=False):
+    if jwt:
+        request = requests.Request()
+        userinfo = id_token.verify_oauth2_token(
+            id_token=access_token, request=request, audience=settings.G_CLIENT_ID
+        )
+    else:
+        session = OAuth2Session(
+            token={"access_token": access_token}, client_id=settings.G_CLIENT_ID
+        )
+        userinfo = session.get("https://www.googleapis.com/oauth2/v1/userinfo").json()
     return userinfo
