a731a249599ec59fb2d1d559fc5d7544a523d7ab
Arijit Basu <sayanarijit@gmail.com>
2024-07-02 13:03:09 +0530

    Feedback commit

--- apphelpers/socialauth/goog.py
+++ apphelpers/socialauth/goog.py
@@ -13,15 +13,15 @@ if not settings.G_REDIRECT_URI.startswith("https"):
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "True"
 
 
-def fetch_info(access_token, jwt=False):
-    if jwt:
-        request = requests.Request()
-        userinfo = id_token.verify_oauth2_token(
-            id_token=access_token, request=request, audience=settings.G_CLIENT_ID
-        )
-    else:
-        session = OAuth2Session(
-            token={"access_token": access_token}, client_id=settings.G_CLIENT_ID
-        )
-        userinfo = session.get("https://www.googleapis.com/oauth2/v1/userinfo").json()
+def fetch_info(access_token):
+    session = OAuth2Session(token={"access_token": access_token})
+    userinfo = session.get("https://www.googleapis.com/oauth2/v1/userinfo").json()
+    return userinfo
+
+
+def fetch_info_using_jwt(access_token):
+    request = requests.Request()
+    userinfo = id_token.verify_oauth2_token(
+        id_token=access_token, request=request, audience=settings.G_CLIENT_ID
+    )
     return userinfo
